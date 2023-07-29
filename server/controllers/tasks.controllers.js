a0a500cd350bcceebff5b34542f5cf63f3ebaccd
julio cesar fuentes <juliocesarfuentesfuentes16@gmail.com>
2023-07-29 16:03:44 -0400

    backend terminado obtener actualizar eliminar y agregar

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -2,42 +2,80 @@ import { pool } from "../db.js";
 
 
 
-export const getTasks = (req , res) =>{
-    res.send('obteniendo todas las tareas');
+export const getTasks = async(req , res) =>{
+    try{
+        const [result] = await pool.query("select * from tasks order by createAt asc");
+        res.json(result);
+        
+     
+    }catch(ERROR){
+        console.error(ERROR)
+    }
+   
 }
 
 
-export const getTask = (req , res) =>{
-    res.send('obteniendo una tarea');
+export const getTask = async(req , res) =>{
+
+   try{
+        const [result] = await pool.query("select * from tasks where id = ?",[req.params.id,]);
+        res.json(result[0])
+        
+   }catch(ERROR){
+        console.log(ERROR);
+   }
+        
 }
 
 
 export const createTask = async(req , res) =>{
-    //req.body resibe datos desde el body
-    console.log(req.body);
-    
-    //obtengo los valores de req.body
-    const {title,descripcion} = req.body;
 
-    //sintaxis mysql insertando datos a la tabla tasks
-    const [result] = await pool.query(
-        "INSERT INTO tasks(title, descripcion) values (?,?)",
-        [title,descripcion]
-            
+   try{
+        //req.body resibe datos desde el body
+        console.log(req.body);
+        //obtengo los valores de req.body
+        const {title,descripcion} = req.body;
+        //sintaxis mysql insertando datos a la tabla tasks
+        const [result] = await pool.query(
+        "INSERT INTO tasks(title, descripcion) values (?,?)",[title,descripcion]);
         
-    );
-    console.log(result);
+        res.json({id:result.insertId,title,descripcion,});
 
-    res.json({id:result.insertId,title,descripcion,});
+   }catch(ERROR){
+
+        console.error(ERROR);
+
+   }
+
+    
 }
 
 
-export const updateTask = ( req , res) =>{
-    res.send('actualizando tareas');
+export const updateTask = async( req , res) =>{
+
+    try{
+
+        const {title, descripcion} = req.body;
+        const [result] = await pool.query("update tasks set title = ? , descripcion = ? where id = ?",[title,descripcion,req.params.id,]);
+        res.json(result[0]);
+        
+
+    }catch(ERROR){
+        console.error(ERROR);
+    }
+ 
+    
 }
 
 
-export const deleteTask = (req, res) => {
-    res.send('eliminado tareas');
+export const deleteTask = async(req, res) => {
+    
+    try{
+        const [result] = await pool.query("delete from tasks where id = ?",[req.params.id,]);
+        res.json(result[0]);
+    }catch(ERROR){
+        console.error(ERROR);
+    }
+    
 }
 
--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ import express from "express";
 import{PORT}  from "./config.js";
 import indexRoutes from './routes/index.routes.js'
 import taskRouters from './routes/task.routes.js'
+import cors from "cors";
 
 
 const app = express();
+
+//con esta libreria puedes obtener acceso al servidor desde el front
+app.use(cors());
 
 //para recibir datos desde body
 app.use(express.json());
